ba7db733d6101d6a66116c38aab3f86ae331489c
vpiyanov <vladimir.piyanov@gmail.com>
2025-08-04 23:56:11 +0200

    Update uninstall_outline.sh

--- uninstall_outline.sh
+++ uninstall_outline.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
-/etc/init.d/tun2socks disable
+if [ -f "/etc/init.d/tun2socks" ]; then
+  /etc/init.d/tun2socks disable
+  rm /etc/init.d/tun2socks
+fi
+if [ -f "/etc/hotplug.d/iface/99-restart-tun2socks" ]; then
+  rm /etc/hotplug.d/iface/99-restart-tun2socks
+fi
+if [ -f "/usr/bin/tun2socks" ]; then
+  rm /usr/bin/tun2socks
+fi
 
-echo "Deleting interface 'tunnel'"
-uci delete network.tunnel
-echo "Deleting firewall zone 'proxy'"
-uci del firewall.$(uci show firewall | grep "name='proxy'" | cut -d. -f2)
-echo "Deleting firewall forwarding 'lan-proxy'"
-uci del firewall.$(uci show firewall | grep "name='lan-proxy'" | cut -d. -f2)
 
+if grep -q "config interface 'tunnel'" /etc/config/network; then
+  echo "Deleting interface 'tunnel'"
+  uci delete network.tunnel
+fi
+if grep -q "option name 'proxy'" /etc/config/firewall; then 
+  echo "Deleting firewall zone 'proxy'"
+  uci del firewall.$(uci show firewall | grep "name='proxy'" | cut -d. -f2)
+  echo "Deleting firewall forwarding 'lan-proxy'"
+  uci del firewall.$(uci show firewall | grep "name='lan-proxy'" | cut -d. -f2)
+fi
 uci commit
-
-echo 'Deleting tun2socks....'
-rm /etc/init.d/tun2socks
-rm /etc/hotplug.d/iface/99-restart-tun2socks
-rm /usr/bin/tun2socks
 
 echo 'Restarting Network....'
 /etc/init.d/network restart
