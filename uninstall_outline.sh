87610cefdeba650dbd1ea27d257ef55aef7fac1c
vpiyanov <vladimir.piyanov@gmail.com>
2025-08-04 23:31:19 +0200

    Update uninstall_outline.sh

--- uninstall_outline.sh
+++ uninstall_outline.sh
@@ -1,1 +1,12 @@
+#!/bin/sh
+
+echo "Deleting interface 'tunnel'"
+uci delete network.tunnel
+
+echo "Deleting firewall zone 'proxy'"
+uci del firewall.$(uci show firewall | grep "name='proxy'" | cut -d. -f2)
+
+echo "Deleting firewall forwarding 'lan-proxy'"
+uci del firewall.$(uci show firewall | grep "name='lan-proxy'" | cut -d. -f2)
+
 
