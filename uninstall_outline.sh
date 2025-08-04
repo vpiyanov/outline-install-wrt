84ec4b0fd55cba636ce33d6aa86d1a8eab7996b5
vpiyanov <vladimir.piyanov@gmail.com>
2025-08-04 23:48:35 +0200

    Update uninstall_outline.sh

--- uninstall_outline.sh
+++ uninstall_outline.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+/etc/init.d/tun2socks stop
+/etc/init.d/tun2socks disable
+
 echo "Deleting interface 'tunnel'"
 uci delete network.tunnel
 
@@ -8,6 +11,8 @@ uci del firewall.$(uci show firewall | grep "name='proxy'" | cut -d. -f2)
 
 echo "Deleting firewall forwarding 'lan-proxy'"
 uci del firewall.$(uci show firewall | grep "name='lan-proxy'" | cut -d. -f2)
+
+uci commit
 
 rm /etc/init.d/tun2socks
 rm /etc/hotplug.d/iface/99-restart-tun2socks
